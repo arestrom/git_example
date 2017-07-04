008e6c1c4660c13258e1d66c17a1eb3bb7d71341
Are Strom <are.strom@gmail.com>
2017-07-03 21:50:07 -0700

    Added text

--- GitSetupInstructions.R
+++ GitSetupInstructions.R
@@ -11,24 +11,21 @@ file.exists("C:\\Users\\stromas\\.ssh\\id_rsa.pub")
 
 # Check status of branch
 # git remote -v
-# origin git@github.com:arestrom/reack.git (fetch)
-# origin git@github.com:arestrom/reack.git (push)
+# origin git@github.com:arestrom/git_example.git (fetch)
+# origin git@github.com:arestrom/git_example.git (push)
 
 # Connect new branch to remote github repository
 # Remote repo exists, remote branch does not exist
-# git remote add upstream git@github.com:arestrom/reack.git
+# Connect back to master first
+# git checkout master
+# git push --set-upstream origin new-feature
 
 # Re-check status of branch
 # git remote -v
-# origin git@github.com:arestrom/reack.git (fetch)
-# origin git@github.com:arestrom/reack.git (push)
-# upstream      git@github.com:arestrom/reack.git (fetch)
-# upstream      git@github.com:arestrom/reack.git (push)
+# origin git@github.com:arestrom/git_example.git (fetch)
+# origin git@github.com:arestrom/git_example.git (push)
 
 # Delete branch. Big D permanent delete
 # git branch -D new-feature
 # Deleted branch new-feature (was 05d3cc5)
 
-# Added text
-
-# Added more text ....new feature branch
