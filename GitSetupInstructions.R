b8ac6645b9f558c3aa529832f17e1fe881498fdd
Are Strom <are.strom@gmail.com>
2017-07-03 21:06:05 -0700

    Initial commit

---
+++ GitSetupInstructions.R
@@ -0,0 +1,27 @@
+# See: http://eriqande.github.io/rep-res-web/lectures/git-branch-and-merge.html
+
+# Create and switch to a new branch
+# git checkout -b new-branch
+# Switched to a new branch "new-branch"
+
+# Check status of branch
+# git remote -v
+# origin git@github.com:arestrom/reack.git (fetch)
+# origin git@github.com:arestrom/reack.git (push)
+
+# Connect new branch to remote github repository
+# Remote repo exists, remote branch does not exist
+# git remote add upstream git@github.com:arestrom/reack.git
+
+# Re-check status of branch
+# git remote -v
+# origin git@github.com:arestrom/reack.git (fetch)
+# origin git@github.com:arestrom/reack.git (push)
+# upstream      git@github.com:arestrom/reack.git (fetch)
+# upstream      git@github.com:arestrom/reack.git (push)
+
+# Delete branch. Big D permanent delete
+# git branch -D new-feature
+# Deleted branch new-feature (was 05d3cc5)
+
+# Added text
